151f18554961da2439c312e22e3159f914cd075a
React Native <jimmydelosangeles@gmail.com>
2018-11-26 01:51:46 -0500

    got snake head to move one dirrection

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("JS is running");
     
     let gridContainer = $("#container");
+    let snakeHead = document.querySelector("#snake");
+    let boxSize;
+
 
     function drawBoxes(){
         let groundSize = parseInt($("#container").css("height"));
         let ratio = 700/groundSize;
-        let boxSize = 20 / ratio;
+        boxSize = (20 / ratio);
         let totalBoxes = ((groundSize * groundSize)/(boxSize * boxSize)).toFixed(0);
         for (let i = 0; i < totalBoxes ; i++) {
             let box = document.createElement("DIV")
@@ -23,6 +26,57 @@ document.addEventListener("DOMContentLoaded", function () {
             box.style.width = boxSize;
             document.querySelector("#container").appendChild(box);
         }
+        drawSnake();
+    }
+
+    function drawSnake(){
+        //get snake head
+        snakeHead.style.height = boxSize;
+        snakeHead.style.width = boxSize;
+        // get center position
+        let center = $("#612")[0];
+        let centerY = parseInt(center.offsetTop);
+        let centerX = parseInt(center.offsetLeft);
+        // give center position to snake
+        snakeHead.style.left = centerX;
+        snakeHead.style.top = centerY;
+        
+        setInterval(moveSnake, 1000);
+       
+
+    }
+    
+    function moveSnake(){  
+        let moveY = boxSize
+        let moveX = boxSize
+        let snakeX = parseInt(snakeHead.style.left);
+        let snakeY = parseInt(snakeHead.style.top);
+        snakeHead.style.left = snakeX + moveX;
+        
+        function checkKey(e) {
+
+            e = e || window.event;
+
+            if (e.keyCode == '38') {
+                // up arrow
+                moveX = -moveX;
+            }
+            else if (e.keyCode == '40') {
+                // down arrow
+                moveX = -moveX;
+            }
+            else if (e.keyCode == '37') {
+                // left arrow
+                moveY = -moveY;
+            }
+            else if (e.keyCode == '39') {
+                // right arrow
+                moveY = -moveY;
+            }
+
+        }
+
+
     }
 
     drawBoxes();
